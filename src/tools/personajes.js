a2459e0c4aae38f96448ba0a4a373ff75ba05e60
AmilcarD99 <rodisamilcardm@gmail.com>
2024-08-07 19:38:11 -0600

    nuevas modificaciones de estilo en las cartas

--- src/tools/personajes.js
+++ src/tools/personajes.js
@@ -1,4 +1,74 @@
 export const personajes = [
-    'Rodis',
-    'cesar'
+    {
+        personaje: 'Iron',
+        img: '/imagenes/iron.jpeg',
+        poderes: [
+            {
+                poder: 'caracteristicas Fisicas sobre Humanas'
+            },
+            {
+                poder: 'Manipulacion Mental',
+                descripcion: 'Cuando usa provocacion sus enemigos pierden la cabeza y van hacia el enfadados'
+            },
+            {
+                poder: 'Dominio de Armas'
+            },
+            {
+                poder: 'Regeneracion',
+                descripcion: 'Divina-Baja mientras sung in-Woo tenga mana seguira regenerando indifinidamente'
+            },
+            {
+                poder: 'inmortalidad',
+                descripcion: 'tipos 1,2,3,7 y 8'
+            },
+            {
+                poder: 'Desarrollo Acelerado y ampliacion de amplificacion de estadisticas',
+                descripcion: 'fuerza de ataque, velocidad, Fuerza de levantamiento, Resistencia'
+            }
+
+        ]
+    },
+    {
+        personaje: 'Igris',
+        img: '/imagenes/arise.jpg',
+        poderes: [
+            {
+                poder: 'Regeneracion',
+                descripcion: 'Divina-Baja. Mientras Sung in-Woo tenga maná se seguirá regenerando indefinidamente.'
+            },
+            {
+                poder: 'Inmortalidad',
+                descripcion: 'Tipos 1, 2, 3, 7 y 8'
+            },
+            {
+                poder: 'Desarrollo Acelerado y Amplificación de Estadísticas',
+                descripcion: 'Las sombras de Sung Jin-Woo pueden subir de nivel y se hacen más fuertes cuando Sung aumenta su inteligencia. Puede aumentar en un 50% sus estadísticas cuando Sung usa el dominio del Monarca'
+            },
+            {
+                poder: 'Autosuficiencia',
+                descripcion: 'Tipos 1, 2 y 3'
+            },
+            {
+                poder: 'Velocidad',
+                descripcion: ' Hipersónica Alta+ (Relativo a Sung Jin-Woo en el Arco del Portal Rojo y podía presionar a Baruka), superior con Desarrollo Acelerado y bajo el Dominio del Monarca'
+            },
+            {
+                poder: 'Fuerza de Levantamiento',
+                descripcion: ' Clase K, superior con Desarrollo Acelerado y bajo el Dominio del Monarca.',
+            },
+            {
+                poder: 'Equipamiento',
+                descripcion: 'Una gran espada de varios metros de largo, dos dagas y armadura',
+            },
+
+
+        ]
+    },
+    {
+        personaje: '',
+        img: '/imagenes/Sung.jpg',
+        poderes: [
+
+        ]
+    }
 ]
