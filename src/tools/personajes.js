43320e179ddadea5235a589e46616c8206247a11
AmilcarD99 <rodisamilcardm@gmail.com>
2024-08-13 15:58:06 -0600

    se realizaron cambios en el navbar

--- src/tools/personajes.js
+++ src/tools/personajes.js
@@ -65,10 +65,141 @@ export const personajes = [
         ]
     },
     {
-        personaje: '',
+        personaje: 'Sung Jinwoo',
         img: '/imagenes/Sung.jpg',
         poderes: [
+            {
+                poder: 'Sigilio',
+                descripcion: 'Jinwoo es capaz de camuflarse con su entorno y ocultar todos los rastros de su presencia, convirtiéndose esencialmente en invisible tanto física como mágicamente'
+            },
+            {
+                poder: 'Sed de sangre',
+                descripcion: ' Jinwoo es capaz de intimidar mágicamente a un combatiente enemigo para que se someta, induciendo un miedo intenso en aquellos que son atacados por esta técnica y disminuyendo sus estadísticas en un 50%. También se puede usar en varios oponentes a la vez.'
+            },
+            {
+                poder: 'Azogue',
+                descripcion: 'Jinwoo es capaz de aumentar su velocidad en un 30% durante un corto período de tiempo.'
+            },
+            {
+                poder: 'Mutilación',
+                descripcion: 'Jinwoo apunta a los puntos vitales de un objetivo antes de cortarlo con sus dagas, infligiendo daño crítico'
+            },
+            {
+                poder: 'Ataque de daga',
+                descripcion: 'Jinwoo bombardea a un objetivo con sus dagas desde todas las direcciones'
+            },
+            {
+                poder: 'Autoridad del gobernante',
+                descripcion: 'Jinwoo es capaz de mover y controlar objetos a través de la telequinesis. También puede usar esta habilidad en el aire a su alrededor, lo que le permite moverse y esquivar ataques mientras está en el aire.'
+            },
+            {
+                poder: 'Dominio del Monarca:',
+                descripcion: 'Jinwoo es capaz de aumentar la fuerza de todas sus sombras activas en un 50% en la batalla'
+            },
+            {
+                poder: 'Extracción de sombras',
+                descripcion: 'Jinwoo es capaz de extraer sombras de los cadáveres de sus enemigos y añadirlas a su ejército'
+            }
+
 
         ]
+    },
+    {
+        personaje: 'Cha Hae-In',
+        img: '/imagenes/chahaein.png',
+        poderes: [
+            {
+                poder: 'Olor a maná',
+                descripcion: ' Cha es sensible al olor del maná, lo que hace que los cazadores y los monstruos le huelan mal. Jinwoo es el único cazador conocido que no huele mal a ella, debido a su condición de Jugador del Sistema.'
+            },
+            {
+                poder: 'Dominio de la espada',
+                descripcion: ' Cha es competente en el manejo de la espada y es capaz de idear formas creativas y poco ortodoxas de matar monstruos de manera eficiente. Desafortunadamente, sus habilidades no son rival contra oponentes inmensos como Kandiaru, cuya maestría era mucho más poderosa que la habilidad de Jinwoo'
+            },
+            {
+                poder: 'Velocidad mejorada',
+                descripcion: ' Cha puede moverse a grandes velocidades y es bien conocida entre la comunidad de cazadores coreana por lo rápida que es en la batalla.'
+            },
+            {
+                poder: 'Espada de Luz',
+                descripcion: 'Hae-In transforma su espada en luz pura, aumentando significativamente su poder de corte'
+            },
+            {
+                poder: 'Terremoto de provocación',
+                descripcion: 'Hae-In clava su espada en el suelo, creando un pequeño terremoto que daña a cualquier enemigo atrapado en su radio.'
+            }
+
+        ]
+    },
+    {
+        personaje: 'Choi Jong-In',
+        img: '/imagenes/Choi.png',
+        poderes: [
+            {
+                poder: 'Magia de fuego',
+                descripcion: 'Choi es el mejor usuario de fuego en Corea y se rumorea que puede destruir un edificio alto con un solo hechizo'
+            },
+            {
+                poder: 'Durabilidad mejorada',
+                descripcion: 'Choi posee una gran durabilidad física a pesar de ser un arquetipo de mago'
+            },
+            {
+                poder: 'Velocidad mejorada',
+                descripcion: ' Choi puede moverse a grandes velocidades, como lo demuestra la forma en que pudo esquivar por poco la visión de calor de la Estatua de Dios.'
+            },
+            {
+                poder: ' Choi puede moverse a grandes velocidades, como lo demuestra la forma en que pudo esquivar por poco la visión de calor de la Estatua de Dios.',
+                descripcion: 'Choi tiene una enorme reserva de maná, que supera a la mayoría de los personajes, que se muestra en su capacidad para mantener una gran potencia de fuego durante toda la incursión de la isla de Jeju'
+            },
+            {
+                poder: 'Lanza de fuego',
+                descripcion: ' Choi crea varias lanzas de fuego y las lanza como sus enemigos'
+            },
+            {
+                poder: 'Resistencia al fuego',
+                descripcion: 'Como portador de magia de fuego, Choi parece no verse afectado por las altas temperaturas y no puede ser herido por sus propias llamas'
+            },
+            {
+                poder: 'Prisión de la Llama',
+                descripcion: ' Choi es capaz de crear una gran red de llamas tanto con fines ofensivos como defensivos. Según él mismo, es lo suficientemente fuerte como para contener a las bestias mágicas de rango B'
+            },
+            {
+                poder: 'Dragón de llamas',
+                descripcion: 'Choi es capaz de moldear una masa de fuego arremolinada en un pequeño dragón, que luego puede usar para quemar a cualquier enemigo cercano hasta convertirlo en cenizas'
+            }
+
+        ]
+    },
+    {
+        personaje: 'Thomas Andre',
+        img: '/imagenes/thomas.jpg',
+        poderes: [
+            {
+                poder: 'Inmensa fuerza',
+                descripcion: 'Thomas posee una inmensa fuerza física. Como testimonio de esto, fue capaz de enviar a Jinwoo volando con un solo golpe, derribar edificios solo por la presión del viento generada por sus ataques,'
+            },
+            {
+                poder: 'Velocidad inmensa',
+                descripcion: 'Thomas puede moverse a velocidades inmensas. Fue capaz de seguir el ritmo de Jinwoo en su batalla'
+            },
+            {
+                poder: 'Inmensa durabilidad',
+                descripcion: 'Thomas posee una inmensa durabilidad y es capaz de soportar una cantidad ridícula de castigo, incluso de oponentes mucho más fuertes que él'
+            },
+            {
+                poder: 'Refuerzo',
+                descripcion: 'Thomas aumenta drásticamente tanto su poder de defensa como de ataque, cubriéndose de pies a cabeza con una armadura dorada con cuernos negros y dándole a su cabello una calidad brillante como una llama'
+            },
+            {
+                poder: 'Colapso',
+                descripcion: ' Thomas golpea sus puños contra el suelo, creando una onda de choque masiva que destruye a cualquiera que tenga la mala suerte de quedar atrapado en su radio de explosión.'
+            },
+            {
+                poder: 'Golpe de poder',
+                descripcion: 'Thomas infunde su maná en un solo brazo y lanza un puñetazo extremadamente poderoso'
+            }
+
+        ]
+
     }
 ]
